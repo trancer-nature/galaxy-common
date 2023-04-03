ba3b92d85d9073b455815b1612289fd1f7300525
zengming <1540006936@qq.com>
2023-04-03 16:53:58 +0800

    feature: mongo db add config struct

--- mongo/mongo_helper.go
+++ mongo/mongo_helper.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"sync"
 	"time"
 )
 
@@ -12,30 +13,85 @@ type MongoClientConfig struct {
 	Name            string
 	PassWord        string
 	ReplicaSet      string
-	MaxPoolSize     uint64
+	MaxPoolSize     int
 	MinPoolSize     uint64
 	MaxConnIdleTime time.Duration
 }
 
-func NewMongoClient(ctx context.Context, config MongoClientConfig) (*mongo.Client, error) {
+type MongoPool struct {
+	capacity   int
+	host       string
+	name       string
+	pw         string
+	replicaSet string
+	pool       chan *mongo.Client
+	sync.Mutex
+}
+
+func NewMongoPool(config MongoClientConfig) (*MongoPool, error) {
+	pool := &MongoPool{
+		capacity:   config.MaxPoolSize,
+		host:       config.Url,
+		pw:         config.PassWord,
+		pool:       make(chan *mongo.Client, config.MaxPoolSize),
+		replicaSet: config.ReplicaSet,
+	}
+
+	for i := 0; i < config.MaxPoolSize; i++ {
+		client, err := pool.createClient()
+		if err != nil {
+			return nil, err
+		}
+		pool.pool <- client
+	}
+
+	return pool, nil
+}
+
+func (p *MongoPool) createClient() (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	credential := options.Credential{
-		Username: config.Name,
-		Password: config.PassWord,
+		Username: p.name,
+		Password: p.pw,
 	}
-	clientOptions := options.Client().ApplyURI(config.Url).SetAuth(credential).SetMaxConnIdleTime(config.MaxConnIdleTime)
+	clientOptions := options.Client().ApplyURI(p.host).SetAuth(credential)
 
-	if config.MaxPoolSize > 0 {
-		clientOptions.SetMaxPoolSize(config.MaxPoolSize)
-	}
-
-	if config.MinPoolSize > 0 {
-		clientOptions.SetMinPoolSize(config.MinPoolSize)
-
-	}
-
-	if config.ReplicaSet != "" {
-		clientOptions.SetReplicaSet(config.ReplicaSet)
+	if p.replicaSet != "" {
+		clientOptions.SetReplicaSet(p.replicaSet)
 		clientOptions.SetDirect(false)
 	}
-	return mongo.Connect(ctx, clientOptions)
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func (p *MongoPool) GetClient() (*mongo.Client, error) {
+	select {
+	case client := <-p.pool:
+		return client, nil
+	default:
+		client, err := p.createClient()
+		if err != nil {
+			return nil, err
+		}
+		return client, nil
+	}
+}
+
+func (p *MongoPool) ReleaseClient(client *mongo.Client) {
+	p.Lock()
+	defer p.Unlock()
+
+	if len(p.pool) >= p.capacity {
+		client.Disconnect(context.Background())
+		return
+	}
+
+	p.pool <- client
 }

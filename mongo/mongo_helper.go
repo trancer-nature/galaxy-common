c7cc4644e25a0235c362ed0ebcb856704acf32eb
zengming <1540006936@qq.com>
2023-04-03 15:02:51 +0800

    feature: mongo db add config struct

--- mongo/mongo_helper.go
+++ mongo/mongo_helper.go
@@ -4,17 +4,37 @@ import (
 	"context"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
 )
 
-func NewMongoClient(ctx context.Context, name, password, url, replicaSet string) (*mongo.Client, error) {
-	credential := options.Credential{
-		Username: name,
-		Password: password,
-	}
-	clientOptions := options.Client().ApplyURI(url).SetAuth(credential)
+type MongoClientConfig struct {
+	Url             string
+	Name            string
+	PassWord        string
+	ReplicaSet      string
+	MaxPoolSize     uint64
+	MinPoolSize     uint64
+	MaxConnIdleTime time.Duration
+}
 
-	if replicaSet != "" {
-		clientOptions.SetReplicaSet(replicaSet)
+func NewMongoClient(ctx context.Context, config MongoClientConfig) (*mongo.Client, error) {
+	credential := options.Credential{
+		Username: config.Name,
+		Password: config.PassWord,
+	}
+	clientOptions := options.Client().ApplyURI(config.Url).SetAuth(credential).SetMaxConnIdleTime(config.MaxConnIdleTime)
+
+	if config.MaxPoolSize > 0 {
+		clientOptions.SetMaxPoolSize(config.MaxPoolSize)
+	}
+
+	if config.MinPoolSize > 0 {
+		clientOptions.SetMinPoolSize(config.MinPoolSize)
+
+	}
+
+	if config.ReplicaSet != "" {
+		clientOptions.SetReplicaSet(config.ReplicaSet)
 		clientOptions.SetDirect(false)
 	}
 	return mongo.Connect(ctx, clientOptions)
